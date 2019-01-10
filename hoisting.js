d1a0d5ae2d00b8143b1bc49c2b58e005fa47fec4
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-10 22:17:39 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -22,10 +22,10 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
+  sayMy();
+
   function sayMy() {
     var name = "Kristin";
     console.log(name);
   }
-
-  sayMy();
 }
