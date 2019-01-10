976a384034e2c668c644aca3a175d6d4c93260ab
Marta Kandova <kmartaaaa1989@gmail.com>
2019-01-10 22:16:48 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,14 +1,17 @@
-function callMe(){
+function callMe() {
   var lyric = "maybe";
+
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+
   return lyric;
 }
 
-function crazy(){
-  //fix the code in here:
+
+function crazy() {
+  // fix the code in here:
   var thisIsCrazy = function (){
     console.log("hey!!!")
   }
@@ -19,11 +22,10 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
-
   function sayMy() {
     var name = "Kristin";
-
     console.log(name);
   }
+
+  sayMy();
 }
